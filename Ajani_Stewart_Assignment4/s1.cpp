ce495132fa4de93a2d47b673c8a9c8f8fdb063ea
Ajani Stewart <theamjaha@gmail.com>
2020-04-23 12:25:51 -0400

    completes s1 and s2

---
+++ Ajani_Stewart_Assignment4/s1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "image.h"
+
+using namespace ComputerVisionProjects;
+using namespace std;
+
+Image* create_binary_image(Image* in_image, size_t threshold) {
+  Image* out_image = new Image;
+  out_image->AllocateSpaceAndSetSize(in_image->num_rows(),in_image->num_columns());
+  out_image->SetNumberGrayLevels(255);
+
+  for (size_t row = 0; row < in_image->num_rows(); row++) {
+    for (size_t col = 0; col < in_image->num_columns(); col++){
+      if (in_image->GetPixel(row,col) > threshold) {
+        out_image->SetPixel(row,col,255);
+      } else {
+        out_image->SetPixel(row,col,0);
+      } // end if 
+    } //end for
+  } //end for
+
+  return out_image;
+} // end create_binary_image
+
+
+void get_sphere_info(Image* image, double& x_center, double& y_center, double& radius) {
+  //find center then radius
+  size_t area = 0;
+  for (size_t i = 0; i < image->num_rows(); ++i) {
+    for (size_t j = 0; j < image->num_columns(); ++j) {
+      if (image->GetPixel(i,j)) {
+        x_center += j;
+        y_center += i;
+        area++;
+      } //end if
+    }//end for
+  }// end for
+  x_center = x_center / area;
+  y_center = y_center / area;
+
+  // cout << "x: " << x_center << " y: " << y_center <<endl;
+
+  //for radius, average the distances from the top, bottom, left, right points on circumferences
+  radius = 0;
+  //right
+  auto cur = x_center;
+  while (image->GetPixel(y_center,cur)) {
+    cur++;
+  } // end while
+  radius += (cur - x_center);
+
+  //left
+
+  cur = x_center;
+  while (image->GetPixel(y_center,cur)) {
+    cur--;
+  } // end while
+  radius += (x_center - cur);
+
+  //top
+  cur = y_center;
+  while (image->GetPixel(cur, x_center)) {
+    cur++;
+  } // end while
+  radius += (cur - y_center);
+
+  //bottom
+
+  cur = y_center;
+  while (image->GetPixel(cur, x_center)) {
+    cur--;
+  } // end while
+  radius += (y_center - cur);
+
+  radius /= 4;
+} // end get_sphere_info
+
+void write_items_to_file(string filename, double x, double y, double r) {
+  ofstream f (filename);
+  f << x << " " << y << " " << r << "\n";
+  f.close();
+}
+
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    cout << "usage: ./s1 [input-original-image] [input-threshold-value] [output params file]\n";
+    return EXIT_SUCCESS;
+  }
+
+  Image* input_image = new Image;
+  if (!ReadImage(argv[1], input_image)) {
+    cout << "could not read image: " << argv[1] << "\n";
+    return EXIT_FAILURE;
+  }
+  int threshold;
+  try {
+    threshold = stoi(argv[2]);
+  } catch (exception& e) {
+    cout << "invalid value entered for threshold: " << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+  Image* binary_image = create_binary_image(input_image,threshold);
+  double x_center, y_center, radius;
+  get_sphere_info(binary_image,x_center,y_center,radius);
+  write_items_to_file(argv[3],x_center,y_center,radius);
+  return EXIT_SUCCESS;
+  
+} // end main
---
+++ Ajani_Stewart_Assignment4/s2.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "image.h"
+
+using namespace ComputerVisionProjects;
+using namespace std;
+
+
+void find_brightest_spot(Image* image, int& bx, int& by) {
+  bx = 0, by = 0;
+  // rows are y, cols are x
+
+  for (size_t i = 0; i < image->num_rows(); ++i) {
+    for (size_t j = 0; j < image->num_columns(); ++j) {
+      if (image->GetPixel(i,j) > image->GetPixel(by,bx)) {
+        bx = j;
+        by = i;
+      } //end if
+    } // end for
+  } //end for
+} //end find_brightest_spot
+
+typedef struct {
+  double x;
+  double y;
+  double z;
+} n_vec;
+
+n_vec find_surface_normal(Image* image, int x, int y, int x_center, int y_center, double radius) {
+
+  n_vec n;
+  x -= x_center;
+  y -= y_center;
+
+  cout << (radius*radius)-(x*x)-(y*y) << "\n";
+  n.x = x/(sqrt(radius*radius-x*x-y*y));
+  n.y = y/(sqrt(radius*radius-x*x-y*y));
+  n.z = 1;
+
+
+  //normalize
+  n.x = n.x/(sqrt(n.x*n.x+n.y*n.y+1));
+  n.y = n.y/(sqrt(n.x*n.x+n.y*n.y+1));
+  n.z = n.z/(sqrt(n.x*n.x+n.y*n.y+1));
+
+
+  //scale
+  n.x = n.x * image->GetPixel(y+y_center,x+x_center);
+  //flipped to obey right hand rule
+  n.y = n.y * image->GetPixel(y+y_center,x+x_center) * -1;
+  n.z = n.z * image->GetPixel(y+y_center,x+x_center);
+  return n;
+} // end find_surface_normal
+
+vector<double> split (const string& s) {
+  vector<double> res;
+
+  size_t last = 0;
+  
+  for (size_t i = 0; i < s.size(); i++) {
+    if (s[i] == ' ' || s[i] == '\n') {
+      try {
+        res.push_back(stod(s.substr(last,i-last+1)));
+      } catch (exception& e) {
+        //breaks when there is a " \n"
+        //and ill be damned if i will fixed it
+      }
+      last = i + 1;
+    }
+  }
+
+  res.push_back(stod(s.substr(last,s.size()-last+1)));
+
+  return res;
+}
+
+void read_params_from_file (string filename, vector<double>& params) {
+  ifstream file(filename);
+  string line;
+  getline(file,line);
+  params = split(line);
+  cout << line << endl;
+  file.close();
+}
+
+
+void write_normals_to_file(string filename, const vector<n_vec>& normals) {
+  ofstream file(filename);
+  for (const auto& normal : normals) {
+    file << normal.x << " " << normal.y << " " << normal.z << "\n";
+  }
+  file.close();
+}
+
+
+
+int main(int argc, char** argv) {
+  if (argc != 6) {
+    cout << "usage: ./s2 [input params file] [image1] [image2] [image3] [output directions file]\n";
+    return EXIT_SUCCESS;
+  }
+
+  vector<double> params;
+  read_params_from_file(argv[1], params);
+  Image* image1 = new Image;
+  Image* image2 = new Image;
+  Image* image3 = new Image;
+
+  if (!ReadImage(argv[2], image1)) {
+    cout << "could not read file: " << argv[2] << "\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!ReadImage(argv[3],image2)) {
+    cout << "could not read file: " << argv[3] << "\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!ReadImage(argv[4],image3)) {
+    cout << "could not read file: " << argv[4] << "\n";
+    return EXIT_FAILURE;
+  }
+
+  vector<Image*> images { image1, image2, image3 };
+  vector<n_vec> normals;
+  for (const auto& image : images) {
+    int bx, by;
+    find_brightest_spot(image,bx,by);
+    auto normal = find_surface_normal(image,bx,by,params[0],params[1],params[2]);
+    normals.push_back(normal);
+  }
+
+  write_normals_to_file(argv[5],normals);
+
+  return EXIT_SUCCESS;
+}// end main
